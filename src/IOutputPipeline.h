1a632bb13960bc8fab2df0a7bc876e43f44587ca
James Stevenson <james@stev.org>
2018-06-05 17:19:07 +0100

    Don't need Start/Stop on IOutputPipeline. Can trigger the PipelineBasic directly

--- src/IOutputPipeline.h
+++ src/IOutputPipeline.h
@@ -1,10 +1,9 @@
 
-class IOutputPipeline {
+class IOutputPipeline : public PipelineBasic {
     public:
         virtual ~IOutputPipeline() { };
 
-        virtual void Start() = 0;
-        virtual void Stop() = 0;
+        virtual void OnPreStart() = 0;
 
         virtual void PushBuffer(GstCaps *caps, GstBuffer *buffer) = 0;
         virtual void SetFileName(const std::string filename) = 0;
